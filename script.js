7db96a654016651f92d4e12f2473a38400837692
jassu4u <try.123things@gmail.com>
2026-07-17 21:53:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// Wait for loading animation
+setTimeout(() => {
 
+    document.getElementById("loading").style.display = "none";
+
+    document.getElementById("passwordScreen").style.display = "flex";
+
+}, 4000);
+
+
+// Password
+function checkPassword() {
+
+    let pass = document.getElementById("password").value;
+
+    if (pass === "1234") {
+
+        alert("Welcome Birthday Boy ❤️");
+
+    }
+
+    else {
+
+        document.getElementById("wrong").innerHTML =
+
+            "Wrong Password 🥺";
+
+    }
+
+}
